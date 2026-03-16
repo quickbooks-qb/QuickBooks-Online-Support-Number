0e02a29edab12c3f364f110c02748b69fa3cd993
Henrry <iamsachinyadav852@gmail.com>
2026-03-16 12:06:00 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,16 @@
+project = 'QuickBooks Online Support Number'
+author = 'QuickBooks'
+release = '1.0'
 
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+html_static_path = ['_static']
+
+html_favicon = '_static/favicon.png'
+
+html_title = "QuickBooks Online Support"
